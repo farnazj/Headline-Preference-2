aac6f37e816857e4b9db993d0a14aee7fa86069c
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2021-12-06 18:48:13 -0800

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:farnazj/Headline-Preference.git main:gh-pages
+git push -f https://github.com/Headline-Preference.git master:gh-pages
 
 cd -
